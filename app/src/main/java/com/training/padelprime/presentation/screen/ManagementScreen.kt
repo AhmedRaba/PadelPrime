e0dc2b715795c92c8e25dbf77cac979af3b5c33d
Ahmed Raba <ahmedrapa91@gmail.com>
2025-01-02 22:39:58 +0200

    Navigate between Screens with BottomNavigation and Navigation Component

--- app/src/main/java/com/training/padelprime/presentation/screen/ManagementScreen.kt
+++ app/src/main/java/com/training/padelprime/presentation/screen/ManagementScreen.kt
@@ -1,10 +1,6 @@
 package com.training.padelprime.presentation.screen
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.navigation.NavController
@@ -16,7 +12,6 @@ fun ManagementScreen(
     modifier: Modifier = Modifier,
     navController: NavController,
 ) {
-
 
 
 }
